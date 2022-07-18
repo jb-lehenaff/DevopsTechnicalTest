ad8867ba1808c4e6a4b1574a7771e8d2eb24c672
lehenaje <jb7507pvb@gmail.com>
2022-07-18 02:12:02 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import boto3
+
+def push_to_s3(event_body, event_client, message_id):
+    s3 = boto3.resource('s3')
+    filepath = event_client + '/' + message_id + '.json'
+    s3.Bucket('bucketname').upload_file(filepath)
+    s3.upload_fileobj(io.BytesIO(event_body.encode("utf-8")), "myfile.txt")
+
+
+def process_event(event):
+    push_to_s3(event['body'], event_client, message_id)
+
+
+def lambda_handler(event, context):
+    try:
+        process_event(event)
+    except Exception:
+        raise Exception("Unknown error")
---
+++ events.js
@@ -0,0 +1,44 @@
+{
+    "method": "POST",
+    "path": "/",
+    "query": {},
+    "client_ip": "1.2.3.4",
+    "url": "https://localhost/",
+    "headers": {
+        "host": "localhost",
+        "content-length": "636",
+        "date": "Thu, 12 May 2022 15:47:47 GMT",
+        "x-settings": "Spideo",
+        "accept": "*/*",
+        "accept-encoding": "gzip, deflate",
+        "content-type": "application/json",
+        "authorization": "Basic dsdsdscoefe65g==",
+        "user-agent": "Spideo/1.0"
+    },
+    "body": {
+        "anonymousId": null,
+        "channel": "server",
+        "context": {
+            "library": {
+                "name": "analytics-python",
+                "version": "1.4.0"
+            }
+        },
+        "event": "Video Content Started",
+        "integrations": {},
+        "messageId": "42280191-0191-475f-a20191-9e032019116",
+        "originalTimestamp": "2022-05-12T15:47:44.600224+00:00",
+        "projectId": "f7zw0191jHVbjn1",
+        "properties": {
+            "asset_id": "content_0002",
+            "timestamp": "2022-05-12T17:47:44.600219"
+        },
+        "receivedAt": "2022-05-12T15:47:45.845Z",
+        "sentAt": "2022-05-12T15:47:45.076Z",
+        "timestamp": "2022-05-12T15:47:45.368Z",
+        "type": "track",
+        "userId": "f4ca124298",
+        "version": 2,
+        "writeKey": "kakLCIKhY3gcfS5nKfsQNQ4CRoaoCWiu"
+    }
+}
